19469d9a1ad54288c5a61f1c0216738a2f364055
Ben Lichtman <ben@THE-MACHINE.local>
2019-10-12 19:22:22 +1100

    improved rope and added example to main

--- src/main.rs
+++ src/main.rs
@@ -1,98 +1,18 @@
-extern crate memmap;
-
 mod rope;
 
-use std::env;
-use std::fs::{File, OpenOptions};
-use memmap::{Mmap, MmapOptions};
-use rope::Rope;
-use std::collections::BTreeMap;
-use std::slice::Iter;
-
-struct Change {
-	position: usize,
-	rope: Rope,
-	deletions: usize,
-}
-
-struct State {
-	file: File,
-	changes: BTreeMap<u64, Change>,
-}
-
-fn open_file(path: &str) -> File {
-	OpenOptions::new()
-	.read(true)
-	.write(true)
-	.create(true)
-	.open(path)
-	.unwrap()
-}
-
-fn get_mapping(file: &File, offset: u64, mapping_size: u64) -> Mmap {
-	let meta = file.metadata().unwrap();
-
-	let length = match meta.len().checked_sub(offset) {
-		Some(x) if x < mapping_size => x,
-		Some(_) => mapping_size,
-		None => 0,
-	};
-
-	unsafe {
-		MmapOptions::new()
-		.offset(offset)
-		.len(length as usize)
-		.map(&file)
-		.unwrap()
-	}
-}
-
-impl State {
-	fn iter_over(&self, from: u64, to: u64) {
-		assert!(from <= to);
-		let mmap = get_mapping(&self.file, from, to - from);
-
-		let s: Vec<u8> = mmap.iter().copied().collect();
-		let s = s.as_slice();
-		let string = std::str::from_utf8(s).unwrap();
-		println!("{:?}", string);
-	}
-
-	fn insert(&mut self, at: u64, data: Vec<u8>) {
-
-	}
-
-	fn remove(&mut self, at: u64, count: u64) {
-
-	}
-}
-
 fn main() {
-	let args: Vec<String> = env::args().collect();
-	if args.len() < 2 {
-		println!("Please provide a file");
-		return;
-	}
-	let path = &args[1];
-	let offset: u64 = if args.len() < 3 {
-		0
-	}
-	else {
-		args[2].parse().unwrap()
-	};
+	let r = rope::Rope::new();
+	println!("{:#?}", r);
 
-	let open_file = open_file(path);
+	r.insert_at(0, "A".as_bytes()).unwrap();
+	println!("{:#?}", r);
 
-	let state = State {
-		file: open_file,
-		changes: BTreeMap::new(),
-	};
+	r.insert_at(0, "B".as_bytes()).unwrap();
+	println!("{:#?}", r);
 
-	loop {
-		let curpos: u64 = 0;
-		let num_bytes = 10000;
+	r.insert_at(0, "C".as_bytes()).unwrap();
+	println!("{:#?}", r);
 
-		let mut ret = String::new();
-		std::io::stdin().read_line(&mut ret);
-	}
+	r.remove_range(2, 3).unwrap();
+	println!("{:#?}", r);
 }
