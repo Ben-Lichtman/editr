ab649121172acfa98e78d79130aa1d3a3e7aea54
Ben Lichtman <b.lichtman@student.unsw.edu.au>
2019-10-24 13:58:15 +1100

    Changed data model to support threads accessing local storage from other threads

--- src/text_server.rs
+++ src/text_server.rs
@@ -1,10 +1,10 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::error::Error;
 use std::io::{BufReader, BufWriter, Read, Write};
 use std::net::{TcpListener, TcpStream, ToSocketAddrs};
 use std::path::{Path, PathBuf};
-use std::sync::{Arc, RwLock};
-use std::thread::spawn;
+use std::sync::{Arc, Mutex, RwLock};
+use std::thread::{current, spawn, ThreadId};
 
 use serde::{Deserialize, Serialize};
 use serde_json;
@@ -39,17 +39,22 @@ enum Message {
 
 struct FileState {
 	rope: Rope,
+	clients: HashSet<ThreadId>,
 }
 
-struct ClientState<'a> {
-	reader: BufReader<&'a TcpStream>,
-	writer: BufWriter<&'a TcpStream>,
+struct ClientState {
+	reader: BufReader<TcpStream>,
+	writer: BufWriter<TcpStream>,
 	canonical_home: PathBuf,
-	files: Arc<RwLock<HashMap<PathBuf, FileState>>>,
+	current_file_loc: Option<PathBuf>,
+	current_file: Option<Rope>,
 }
 
 // Takes a message and the current client's state, processes it, and returns a message to reply with
-fn process_message(state: &mut ClientState, msg: Message) -> (Message, bool) {
+fn process_message(
+	thread_data: &Arc<Mutex<HashMap<ThreadId, ClientState>>>,
+	msg: Message,
+) -> (Message, bool) {
 	match msg {
 		Message::Echo(inner) => (Message::Echo(inner), false),
 		Message::OpenReq(inner) => {
@@ -70,25 +75,48 @@ fn process_message(state: &mut ClientState, msg: Message) -> (Message, bool) {
 }
 
 // The main function run by the client thread
-fn client_thread(mut state: ClientState) -> Result<(), Box<dyn Error>> {
+fn client_thread(
+	thread_id: ThreadId,
+	thread_data: Arc<Mutex<HashMap<ThreadId, ClientState>>>,
+	files: Arc<RwLock<HashMap<PathBuf, FileState>>>,
+) -> Result<(), Box<dyn Error>> {
 	let mut buffer = [0u8; MAX_MESSAGE];
 	loop {
-		let num_read = state.reader.read(&mut buffer)?;
+		let num_read = thread_data
+			.lock()
+			.map_err(|e| "Unable to lock thread data")?
+			.get_mut(&thread_id)
+			.ok_or("Thread local storage does not exist")?
+			.reader
+			.read(&mut buffer)?;
 
 		// Check for a EOF
 		if num_read == 0 {
 			break;
 		}
+
 		let msg: Message = serde_json::from_slice(&buffer[..num_read])?;
-		let (response, exit) = process_message(&mut state, msg);
+		let (response, exit) = process_message(&thread_data, msg);
 		let response_raw = serde_json::to_vec(&response)?;
-		let num_written = state.writer.write(&response_raw)?;
+		let num_written = thread_data
+			.lock()
+			.map_err(|e| "Unable to lock thread data")?
+			.get_mut(&thread_id)
+			.ok_or("Thread local storage does not exist")?
+			.writer
+			.write(&response_raw)?;
 
 		// Check for a EOF
 		if num_written == 0 {
 			break;
 		}
-		state.writer.flush()?;
+		thread_data
+			.lock()
+			.map_err(|e| "Unable to lock thread data")?
+			.get_mut(&thread_id)
+			.ok_or("Thread local storage does not exist")?
+			.writer
+			.flush()?;
 		if exit {
 			// Client has finished connection
 			break;
@@ -104,21 +132,33 @@ pub fn start<A: ToSocketAddrs>(path: &Path, address: A) -> Result<(), Box<dyn Er
 
 	let files: Arc<RwLock<HashMap<PathBuf, FileState>>> = Arc::new(RwLock::new(HashMap::new()));
 
+	let thread_data: Arc<Mutex<HashMap<ThreadId, ClientState>>> =
+		Arc::new(Mutex::new(HashMap::new()));
+
 	for stream_result in listener.incoming() {
 		let canonical_home = canonical_home.clone();
 		let files = files.clone();
+		let thread_data = thread_data.clone();
+
 		spawn(move || {
-			let stream = match stream_result {
-				Ok(s) => s,
-				Err(_) => return,
-			};
-			let state = ClientState {
-				reader: BufReader::new(&stream),
-				writer: BufWriter::new(&stream),
-				canonical_home,
-				files,
-			};
-			client_thread(state).ok();
+			let stream = stream_result.unwrap();
+
+			let thread_id = current().id();
+
+			thread_data.lock().unwrap().insert(
+				thread_id,
+				ClientState {
+					reader: BufReader::new(stream.try_clone().unwrap()),
+					writer: BufWriter::new(stream.try_clone().unwrap()),
+					canonical_home,
+					current_file_loc: None,
+					current_file: None,
+				},
+			);
+
+			client_thread(thread_id, thread_data.clone(), files).unwrap();
+
+			thread_data.lock().unwrap().remove(&thread_id);
 		});
 	}
 
