832c7b0745349f310360049adedb4a9c4798dc7e
Ben-Lichtman <Ben_Lichtman@icloud.com>
2019-10-26 14:23:59 +1100

    moved structures to new places

--- src/text_server.rs
+++ src/text_server.rs
@@ -7,131 +7,13 @@ use std::path::{Path, PathBuf};
 use std::sync::{Arc, Mutex, RwLock};
 use std::thread::{current, spawn, ThreadId};
 
-use serde::{Deserialize, Serialize};
 use serde_json;
 
+use crate::message::{process_message, Message};
 use crate::rope::Rope;
+use crate::state::{FileState, ThreadShared, ThreadState};
 
 const MAX_MESSAGE: usize = 1024;
-
-#[derive(Serialize, Deserialize)]
-struct WriteReqData {
-	offset: usize,
-	data: Vec<u8>,
-}
-
-#[derive(Serialize, Deserialize)]
-struct ReadReqData {
-	offset: usize,
-	len: usize,
-}
-
-#[derive(Serialize, Deserialize)]
-enum Message {
-	Invalid,
-	Echo(Vec<u8>),
-	OpenReq(String),
-	OpenResp(bool),
-	WriteReq(WriteReqData),
-	WriteResp,
-	ReadReq(ReadReqData),
-	ReadResp(Vec<u8>),
-}
-
-struct FileState {
-	rope: Rope,
-	clients: HashSet<ThreadId>,
-}
-
-struct ThreadShared {
-	reader: BufReader<TcpStream>,
-	writer: BufWriter<TcpStream>,
-}
-
-struct ThreadState {
-	thread_id: ThreadId,
-	thread_shared: Arc<RwLock<HashMap<ThreadId, Mutex<ThreadShared>>>>,
-	files: Arc<RwLock<HashMap<PathBuf, FileState>>>,
-	canonical_home: PathBuf,
-	current_file_loc: Option<PathBuf>,
-}
-
-fn open_file(thread_local: &mut ThreadState, path: &str) -> Result<PathBuf, Box<dyn Error>> {
-	// TODO Remove self from bookkeeping of a file already opened
-	// TODO possibly close file that was already opened
-	let path = Path::new(path);
-
-	let canonical_path = path.canonicalize()?;
-
-	// Check that path is valid given client home
-	if !canonical_path.starts_with(&thread_local.canonical_home) {
-		return Err("Invalid file path".into());
-	}
-
-	// Make sure the files hashmap contains this file
-	if !thread_local
-		.files
-		.read()
-		.or(Err("Could not read lock file map"))?
-		.contains_key(&canonical_path)
-	{
-		// Read file
-		let mut buffer = Vec::new();
-		let mut file = File::open(&canonical_path)?;
-		file.read_to_end(&mut buffer)?;
-
-		// Add to rope
-		let rope = Rope::new();
-		rope.insert_at(0, &buffer)?;
-
-		thread_local
-			.files
-			.write()
-			.or(Err("Could not write lock file map"))?
-			.insert(
-				canonical_path.clone(),
-				FileState {
-					rope,
-					clients: HashSet::new(),
-				},
-			);
-	}
-
-	// Add bookkeeping
-	thread_local
-		.files
-		.write()
-		.or(Err("Could not write lock file map"))?
-		.get_mut(&canonical_path)
-		.ok_or("Thread local storage does not exist")?
-		.clients
-		.insert(thread_local.thread_id);
-
-	thread_local.current_file_loc = Some(canonical_path.clone());
-
-	Ok(canonical_path)
-}
-
-// Takes a message and the current client's state, processes it, and returns a message to reply with
-fn process_message(thread_local: &mut ThreadState, msg: Message) -> (Message, bool) {
-	match msg {
-		Message::Echo(inner) => (Message::Echo(inner), false),
-		Message::OpenReq(inner) => match open_file(thread_local, &inner) {
-			Ok(_) => (Message::OpenResp(true), false),
-			Err(_) => (Message::OpenResp(false), false),
-		},
-		Message::WriteReq(inner) => {
-			// TODO Do write
-			(Message::WriteResp, false)
-		}
-		Message::ReadReq(inner) => {
-			// TODO Do read
-			let resp_data = Vec::new();
-			(Message::ReadResp(resp_data), false)
-		}
-		_ => (Message::Invalid, false),
-	}
-}
 
 // The main function run by the client thread
 fn client_thread(thread_local: &mut ThreadState) -> Result<(), Box<dyn Error>> {
