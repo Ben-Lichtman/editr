4ea12599d2fc6b02b481f78b0e8aae702702329e
Tony Kang <8369049+tonnnyK@users.noreply.github.com>
2019-10-31 00:58:02 +1100

    Moved ThreadIO related containers into shared_io_container.rs

--- src/text_server.rs
+++ src/text_server.rs
@@ -2,13 +2,13 @@ use std::collections::HashMap;
 use std::error::Error;
 use std::net::{TcpListener, ToSocketAddrs};
 use std::path::{Path, PathBuf};
-use std::sync::{Arc, Mutex, RwLock};
-use std::thread::{spawn, ThreadId};
+use std::sync::{Arc, RwLock};
+use std::thread::spawn;
 
 use serde_json;
 
 use crate::message::{process_message, Message};
-use crate::state::{FileState, ThreadIO, ThreadState};
+use crate::state::{shared_io_container::SharedIOContainer, FileState, ThreadState};
 
 const MAX_MESSAGE: usize = 1024;
 
@@ -60,19 +60,17 @@ pub fn start<A: ToSocketAddrs>(path: &Path, address: A) -> Result<(), Box<dyn Er
 
 	let files: Arc<RwLock<HashMap<PathBuf, FileState>>> = Arc::new(RwLock::new(HashMap::new()));
 
-	let thread_io: Arc<RwLock<HashMap<ThreadId, Mutex<ThreadIO>>>> =
-		Arc::new(RwLock::new(HashMap::new()));
+	let threads_io: Arc<SharedIOContainer> = Arc::new(SharedIOContainer::new());
 
 	for stream_result in listener.incoming() {
 		let canonical_home = canonical_home.clone();
 		let files = files.clone();
-		let thread_io = thread_io.clone();
+		let threads_io = Arc::clone(&threads_io);
 
 		spawn(move || {
 			let stream = stream_result.unwrap();
 
-			let mut thread_local = ThreadState::new(thread_io, files, canonical_home);
-
+			let mut thread_local = ThreadState::new(threads_io, files, canonical_home);
 			thread_local.insert_thread_io(stream).unwrap();
 
 			client_thread(&mut thread_local).unwrap();
