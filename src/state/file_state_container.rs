d6db08127a88dc7b3de28df58bec9369cfc2e1fc
Tony Kang <8369049+tonnnyK@users.noreply.github.com>
2019-11-01 12:40:59 +1100

    Insert/remove for FileStateContainer

--- src/state/file_state_container.rs
+++ src/state/file_state_container.rs
@@ -12,4 +12,38 @@ impl FileStateContainer {
 			container: RwLock::new(HashMap::new()),
 		}
 	}
+	
+	// Inserts a FileState at path
+	pub fn insert(&self, path: &PathBuf, val: FileState) -> Result<(), Box<dyn Error>>{
+		self.write_op(|mut container| container.insert(path, val))
+		Ok(())
+	}
+	
+	// Removes FileState at path
+	pub fn remove(&self, path: &PathBuf) -> Result<(), Box<dyn Error>>{
+		self.write_op(|mut container| container.remove(path))
+		Ok(())
+	}
+
+	// Applies an op that requires a read lock on the underlying container
+	fn read_op<
+		T,
+		F: FnOnce(RwLockReadGuard<HashMap<PathBuf, FileState>>) -> Result<T, Box<dyn Error>>,
+	>(
+		&self,
+		op: F,
+	) -> Result<T, Box<dyn Error>> {
+		op(self.container.read().map_err(|e| e.to_string())?)
+	}
+	
+	// Applies an op that requires a write lock on the underlying container
+	fn write_op<
+		T,
+		F: FnOnce(RwLockWriteGuard<HashMap<PathBuf, FileState>>) -> Result<T, Box<dyn Error>>,
+	>(
+		&self,
+		op: F,
+	) -> Result<T, Box<dyn Error>> {
+		op(self.container.write().map_err(|e| e.to_string())?)
+	}
 }
