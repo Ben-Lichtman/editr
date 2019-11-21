921661b485e869bb7a7b75584ee09bccbcfe3c50
Ben-Lichtman <Ben_Lichtman@icloud.com>
2019-11-21 15:49:21 +1100

    main no longer needs Path

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,5 +1,3 @@
-use std::path::Path;
-
 use editr::text_server;
 
-fn main() { text_server::start(Path::new("files"), "0.0.0.0:12345").unwrap(); }
+fn main() { text_server::start("files", "0.0.0.0:12345").unwrap(); }
